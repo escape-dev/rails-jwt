4cbfe111c2bdd3e452da50697b0f784ca6796834
Muhammad Hafiz Al Fikri <hafiz170301@gmail.com>
2022-09-09 19:56:29 +0700

    adjust the users_controller and add new login routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,21 @@ class UsersController < ApplicationController
       token = encode_token({ user_id: @user.id })
       render json: { user: @user, token: token}, status: :ok
     else 
-      render json: { error: 'Invalid username or password'}, status: :unprocessable_entity
+      render json: { error: @user.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
+  def login 
+    @user = User.find_by(username: user_params[:username])
+
+    if @user && @user.authenticate(user_params[:password])
+      token = encode_token({ user_id: @user.id})
+      render json: { user: @user, token: token}, status: :ok
+    else
+      render json: { error: 'Invalid username or password'}, status: :unprocessable_entity
+    end
+  end
+  
   private 
 
   def user_params 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   resources :users, only: [:create]
+  
+  post "/login", to: "users#login"
 end
