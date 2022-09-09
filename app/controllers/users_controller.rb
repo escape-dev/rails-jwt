2174bdf7f9ede42682c3969e299a5d1012b517f1
Muhammad Hafiz Al Fikri <hafiz170301@gmail.com>
2022-09-09 19:31:34 +0700

    fix the user_params on users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,6 @@ class UsersController < ApplicationController
   private 
 
   def user_params 
-    params.permit(:username, :password)
+    params.require(:user).permit(:username, :password)
   end
 end
